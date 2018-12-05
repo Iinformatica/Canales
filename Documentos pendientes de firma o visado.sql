4a09b14822ca68bbad75876a447ca12ec8c2d280
Isidro <isidro.almodovar@aspe.es>
2018-12-05 10:21:11 +0100

    Signed-off-by: Isidro <isidro.almodovar@aspe.es>

--- Documentos pendientes de firma o visado.sql
+++ Documentos pendientes de firma o visado.sql
@@ -3,5 +3,5 @@ fs.ASUNTO,fs.ORIGEN, fs.INTERESADO, fs.CATEGORIA, fs.ASIGNADO_A
 from fwk_fsolicv fs, til_exp_expedientes ex
 WHERE fs.ESTADO=2 and
 ex.ejercicio||'/'||ex.numero||'-'||ex.identificador=trim((CASE WHEN INSTR(fs.origen, ' ') = 0 THEN fs.origen ELSE SUBSTR(fs.origen,1,INSTR(fs.origen,' ')) END))
-and rownum <= 15
-and fs.dboid_propietario = [%#V#SC_ACTORDBOID%]
+//and rownum <= 15
+//and fs.dboid_propietario = [%#V#SC_ACTORDBOID%]
